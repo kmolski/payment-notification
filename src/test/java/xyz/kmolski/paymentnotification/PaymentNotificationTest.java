c579f60109538b2f9d7ef0f25f3011ebf73d1a7b
k.molski <k.molski@reply.com>
2024-11-15 10:40:18 +0100

    E2E test

--- src/test/java/xyz/kmolski/paymentnotification/PaymentNotificationTest.java
+++ src/test/java/xyz/kmolski/paymentnotification/PaymentNotificationTest.java
@@ -1,11 +1,36 @@
 package xyz.kmolski.paymentnotification;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
+@SpringBatchTest
 class PaymentNotificationTest {
+
+	@Autowired
+	JobLauncherTestUtils jobLauncherTestUtils;
 
 	public static void main(String[] args) {
 		PaymentNotification.main(args);
+	}
+
+	@Test
+	void stepExecutesCorrectly(@Autowired Job job) {
+		jobLauncherTestUtils.setJob(job);
+		var stepExecution = jobLauncherTestUtils.launchStep("check-payment-and-send");
+		assertEquals("COMPLETED", stepExecution.getExitStatus().getExitCode());
+	}
+
+	@Test
+	void jobExecutesCorrectly(@Autowired Job job) throws Exception {
+		jobLauncherTestUtils.setJob(job);
+		var jobExecution = jobLauncherTestUtils.launchJob();
+		assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
 	}
 }
